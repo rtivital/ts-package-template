f21dc31e31cc8b58f0978f0573484f7711e91188
Vitaly Rtishchev <rtivital@gmail.com>
2024-02-24 15:18:33 +0400

    Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,26 @@
-/* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  parserOptions: {
-    project: './tsconfig.eslint.json',
+  env: {
+    browser: true,
+    es2021: true,
   },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'off',
